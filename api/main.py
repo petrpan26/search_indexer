254a89fca98f2da6545bac1a5393fecaffca1da2
Hoang Phan <phan.minhhoang2606@gmail.com>
2023-11-10 00:06:56 -0500

    Fix structure

---
+++ api/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, UploadFile
+from pydantic import BaseModel
+from .rag.answering_engine import add_document, get_answer
+import tempfile
+
+class QuestionModel(BaseModel):
+    document_id: str
+    question: str
+
+app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
+
+@app.post('/api/answer_question')
+async def answer_question(req: QuestionModel):
+    return {'answer': get_answer(req.question, req.document_id)}
+
+
+@app.post('/api/upload_document')
+async def upload_document(file: UploadFile):
+    with tempfile.NamedTemporaryFile(suffix=file.filename) as tmp:
+        tmp.write(file.file.read())
+        return {'document_id': add_document(tmp.name)}
---
+++ api/rag/answering_engine.py
@@ -0,0 +1,49 @@
+from nltk.tokenize import sent_tokenize
+from .file_helper import read_document_from_file
+import uuid
+from pinecone import Index
+from .llm import fetch_embeddings, synthesize_answer
+from .vectordb import add_document_to_db, fetch_top_paragraphs
+from fastapi import HTTPException
+
+# Split document into paragraph of roughly of max_char size
+def split_document_to_paragraphs(document: str, paragraph_len: int = 1000) -> list[str]:
+    sentences = sent_tokenize(document) # Split the paragraph by sentences
+
+    paragraphs = []
+    paragraph = ''
+    for sentence in sentences:
+        paragraph += ' ' + sentence
+        if len(paragraph) >= paragraph_len:
+            paragraphs.append(paragraph)
+            paragraph = ''
+
+    if len(paragraph) > 0:
+        paragraphs.append(paragraph)
+    
+    return paragraphs
+
+# Add document to the database and return id of the document
+def add_document(filepath: str) -> str:
+    document_text = read_document_from_file(filepath)
+    paragraphs = split_document_to_paragraphs(document_text)
+    if len(paragraphs) == 0:
+        raise HTTPException('404', detail='No text was extracted from the document')
+    embeddings = fetch_embeddings(paragraphs)
+    document_id = str(uuid.uuid4())
+    add_document_to_db(document_id, paragraphs, embeddings)
+    print(document_id)
+    return document_id
+
+def get_answer(question: str, document_id: str):
+    embedding = fetch_embeddings([question])[0]
+    relevant_paragraphs = fetch_top_paragraphs(document_id, embedding)
+    if len(relevant_paragraphs) == 0:
+        raise HTTPException(404, detail='Embedding are not ready yet for this document')
+    return synthesize_answer(question, relevant_paragraphs)
+    
+
+
+
+
+    
