19f9db816c495e8c99cd93c1a2eeab00992648ce
Hoang Phan <hoangphan@Hoangs-MacBook-Pro.local>
2023-11-10 09:36:03 -0500

    test

--- next.config.js
+++ next.config.js
@@ -8,21 +8,7 @@ const nextConfig = {
                     process.env.NODE_ENV === "development"
                         ? "http://127.0.0.1:8000/api/:path*"
                         : "/api/",
-            },
-            {
-                source: "/docs",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8000/api/docs"
-                        : "/api/docs",
-            },
-            {
-                source: "/openapi.json",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:8000/api/openapi.json"
-                        : "/api/openapi.json",
-            },
+            }
         ];
     },
 }
