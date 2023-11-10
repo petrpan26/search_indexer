def0b3fe9510d0d8add2a600c4f63cb61ab2006b
Hoang Phan <phan.minhhoang2606@gmail.com>
2023-11-09 23:37:47 -0500

    Add front-end code

---
+++ next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    rewrites: async () => {
+        return [
+            {
+                source: "/api/:path*",
+                destination:
+                    process.env.NODE_ENV === "development"
+                        ? "http://127.0.0.1:8000/api/:path*"
+                        : "/backend/",
+            },
+            {
+                source: "/docs",
+                destination:
+                    process.env.NODE_ENV === "development"
+                        ? "http://127.0.0.1:8000/docs"
+                        : "/backend/docs",
+            },
+            {
+                source: "/openapi.json",
+                destination:
+                    process.env.NODE_ENV === "development"
+                        ? "http://127.0.0.1:8000/openapi.json"
+                        : "/backend/openapi.json",
+            },
+        ];
+    },
+}
+
+module.exports = nextConfig
