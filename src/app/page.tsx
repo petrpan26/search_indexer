19a8a96a773e03cb1035095c0b5f7f4430255f61
Hoang Phan <hoangphan@Hoangs-MacBook-Pro.local>
2023-11-15 11:59:57 -0500

    Add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import DocumentUploader from '@/components/DocumentUploader';
+import Head from 'next/head';
 
 export default function HomePage() {
   return (
     <div>
+      <Head>
+        <title>Book Reading Bot</title>
+      </Head>
       <DocumentUploader></DocumentUploader>
     </div>
   );
